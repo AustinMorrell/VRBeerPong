6d7aa8e3b49d638168d6adbf370cb5e52117ce6d
Austin Morrell <austinlm19@gmail.com>
2017-06-12 09:33:19 -0500

    a few things I fixed

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,17 +5,28 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     private Vector3 Position;
+    private bool turn;
 
 	// Use this for initialization
 	void Start ()
 	{
 	    Position = transform.position;
+	    turn = true;
 		Events.CupH.AddListener(DestroyBall);
 	}
 
     void DestroyBall()
     {
-        transform.position = Position;
+        if (turn)
+        {
+            transform.position = Position;
+            turn = !turn;
+        }
+        else
+        {
+            transform.position = new Vector3(Position.x + 4, Position.y, Position.z - 1.4f);
+            turn = !turn;
+        }
     }
 	
 	
